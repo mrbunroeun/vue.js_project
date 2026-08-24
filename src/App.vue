b89ac6b10eb0c20d887642b1f6c36a9988937633
mrbunroeun <bunroeunhas@gmail.com>
2026-08-24 17:04:26 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+
+<script setup></script>
+
+<template>
+  <h1 id="app">You did it!{{name}}</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+
+</template>
+<script>
+  const {createApp, ref} = Vue
+  createApp({ 
+    setup(){
+      const name = ref("Rachel")  
+      return {name}
+    }
+  }).mount("#app")
+</script>
+
+<style scoped></style>
