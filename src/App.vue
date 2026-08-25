6b3cbf5ba321a100c5f0a930b24c199ca167a610
mrbunroeun <bunroeunhas@gmail.com>
2026-08-25 23:13:33 +0700

    working port folio

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,9 @@
+<script setup>
+import { ref } from 'vue'
 
-<script setup></script>
-
-<template>
-  <h1 id="app">You did it!{{name}}</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-
-</template>
-<script>
-  const {createApp, ref} = Vue
-  createApp({ 
-    setup(){
-      const name = ref("Rachel")  
-      return {name}
-    }
-  }).mount("#app")
+const name = ref("Rachel")
 </script>
 
-<style scoped></style>
+<template>
+
+</template>
